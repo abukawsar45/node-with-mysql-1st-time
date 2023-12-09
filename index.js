4dfcdd3cf05fe4d3f6834b4ffc35f6f8949c8c26
abukawsar45 <kawsar1146236@gmail.com>
2023-12-09 16:43:22 +0600

    datad delete done

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var mysql = require('mysql');
 
-
 var DatabaseConnectionConfig = {
   host: 'localhost',
-  user:'root',
+  user: 'root',
   password: '',
-  database: 'school'
-}
+  database: 'school',
+};
 
-var con = mysql.createConnection(DatabaseConnectionConfig)
+var con = mysql.createConnection(DatabaseConnectionConfig);
 
-con.connect((e) => {  if (e){console.log('Connection Fail')}
-  else
-  {
-    console.log('Connection Success')
-    InsertData(con)
+con.connect((e) => {
+  if (e) {
+    console.log('Connection Fail');
+  } else {
+    console.log('Connection Success');
+    // InsertData(con);
+    DeleteDataById(con);
   }
 });
 
-
 const InsertData = (con) => {
   let SQLQuery =
-    "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('AbuKawSar', '01', 'ten', '01787', 'Tangail' )";
+    "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Kawsar', '03', 'ten', '01581', 'Dhaka' )";
   con.query(SQLQuery, (err) => {
     if (err) {
       console.log('Data insert Fail');
@@ -30,3 +30,15 @@ const InsertData = (con) => {
     }
   });
 };
+
+
+const DeleteDataById = (con) => {
+  let SQLQuery = "DELETE FROM `students_list` WHERE `id`='3'";
+  con.query(SQLQuery, (error) => {
+    if(error){console.log('Data Delete Fail')}
+    else
+    {
+      console.log('Data Delete Success')
+    }
+  })
+}
