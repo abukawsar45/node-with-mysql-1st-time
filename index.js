ce05aa8a71ea4040365b1a54f348af2832642f0d
abukawsar45 <kawsar1146236@gmail.com>
2023-12-09 15:37:43 +0600

    data insert done

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ var mysql = require('mysql');
 var DatabaseConnectionConfig = {
   host: 'localhost',
   user:'root',
-  password: ''
+  password: '',
+  database: 'school'
 }
 
 var con = mysql.createConnection(DatabaseConnectionConfig)
 
-con.connect((e) => {
-  if (e)
-  {
-    console.log('Connection Fail')
-  }
+con.connect((e) => {  if (e){console.log('Connection Fail')}
   else
   {
     console.log('Connection Success')
+    InsertData(con)
   }
-} );
+});
+
+
+const InsertData = (con) => {
+  let SQLQuery =
+    "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('AbuKawSar', '01', 'ten', '01787', 'Tangail' )";
+  con.query(SQLQuery, (err) => {
+    if (err) {
+      console.log('Data insert Fail');
+    } else {
+      console.log('Data insert Success');
+    }
+  });
+};
